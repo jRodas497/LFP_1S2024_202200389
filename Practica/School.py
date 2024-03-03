eb40d3df7ac92249f4519382c89e47b024d88278
jRodas497 <3615324580101@ingenieria.usac.edu.gt>
2024-03-02 18:15:25 -0600

    Final

--- Practica/School.py
+++ Practica/School.py
@@ -52,3 +52,25 @@ class School:
                 file.write('</p><br>')
             file.write('</body></html>')
         print('Listado hecho!')
+        
+    def approved(self):
+        with open('aprobados.html', 'w') as file:
+            file.write('')
+            file.write('<html><head><style>')
+            file.write(' h1 { color:navy; }')
+            file.write('p { margin-bottom: 15px }')
+            file.write('.approved { color: green, font-weight: bold; }')
+            file.write('.not { color: red, font-weight: bold; }')
+            file.write('</style></head><body><h1>Reporte de Aprobados</h1>')
+            
+            for est in self.students:
+                file.write(f'<p><strong>Carnet: </strong> {est.id} </p>')
+                file.write(f'<p><strong>Nombre: </strong> {est.name} </p>')
+                file.write(f'<p><strong>Carnet: </strong> {est.calc_prom()} </p>')
+                if est.approved():
+                    file.write('<p class = "approved"> Aprobado</p><br>')
+                else:
+                    file.write('<p class = "not"> Reprobado</p><br>')
+            file.write('</body></html>')
+        print('Reporte generado!')
+            
--- Practica/main.py
+++ Practica/main.py
@@ -9,45 +9,55 @@ def main():
     menu()
 
 def menu():
-    print("")
-    print("-------------------------------------------------------")
-    print("#############        MENÚ PRINCIPAL       #############")
-    print("#############      GOTITAS DEL SABER      #############")
-    print("-------------------------------------------------------")
+    program = School()
+    while True:
+        print("")
+        print("-------------------------------------------------------")
+        print("#############        MENÚ PRINCIPAL       #############")
+        print("#############      GOTITAS DEL SABER      #############")
+        print("-------------------------------------------------------")
 
-    print("-------------------------------------------------------")
-    print("| 1. SALIR                                            |")
-    print("| 2. DATOS DEL ALUMNO                                 |")
-    print("| 2. CARGAR ALUMNOS                                   |")
-    print("| 4. AGREGAR CALIFICACIONES                           |")
-    print("| 5. REPORTE ALUMNOS                                  |")
-    print("| 6. REPORTE APROBADOS                                |")
-    print("| 7. TOP 3 ALUMNOS                                    |")
-    print("-------------------------------------------------------")
-    
-    print("")
-    print("##     INGRESE UNA OPCION:     ##")
-    opcion = input()
-    print("")
-    
-    if opcion == '1':
-        print('+++  Adios, esperamos volver a verte :)  +++')
-    elif opcion == '2':
-        data()
+        print("-------------------------------------------------------")
+        print("| 1. SALIR                                            |")
+        print("| 2. DATOS DEL ALUMNO                                 |")
+        print("| 3. CARGAR ALUMNOS                                   |")
+        print("| 4. AGREGAR CALIFICACIONES                           |")
+        print("| 5. REPORTE ALUMNOS                                  |")
+        print("| 6. REPORTE APROBADOS                                |")
+        print("| 7. TOP 3 ALUMNOS                                    |")
+        print("-------------------------------------------------------")
         
-    elif opcion == '3':
-        menu()
-    elif opcion == '4':
-        menu()
-    elif opcion == '5':
-        menu()
-    elif opcion == '6':
-        menu()
-    elif opcion == '7':
-        menu()
-    else:
-        print('Opción no valida :( \n Vuelve a intentarlo')
-        menu()
+        print("")
+        print("##     INGRESE UNA OPCION:     ##")
+        opcion = input()
+        print("")
+        
+        if opcion == '1':
+            print('+++  Adios, esperamos volver a verte :)  +++')
+            break
+        elif opcion == '2':
+            data()
+        elif opcion == '3':
+            name = loadDocument('est')
+            if name:
+                program.load_students(name)
+                
+        elif opcion == '4':
+            name = loadDocument('cal')
+            if name:
+                program.add_ratings(name)
+        elif opcion == '5':
+            program.report()
+            
+        elif opcion == '6':
+            program.approved()
+            
+        elif opcion == '7':
+            program.top3()
+            
+        else:
+            print('Opción no valida :( \n Vuelve a intentarlo')
+            print('')
     
 def data():
     print("-------------------------------------------------------")
@@ -55,8 +65,21 @@ def data():
     print("-------------------------------------------------------")
     print("-> JUAN JOSÉ RODAS MANSILLA                           |")
     print("-> 202200389                                          |")
-    print("->LFP Sección A-                                      |")
+    print("-> LFP Sección A-                                     |")
     print("-> Ingenieria en Ciencias y Sistemas                  |")
     
-    menu()
-main()
+    
+def loadDocument(ext):
+    root = tkin.Tk(); root.attributes('-alpha',0.01)
+    root.attributes('-topmost',True)
+    root.tk.eval(f'tk::PlaceWindow {root._w} center')
+    root.withdraw()
+    name = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes=[(f"{ext} files", f"*.{ext}")])
+    
+    if not name:
+        root.destroy()
+        print('Se ha cancelado')
+        return None
+    
+    root.destroy()
+    return name
